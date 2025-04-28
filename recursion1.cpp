205ea942659e9014595aa08ffce8bc9d6579dd3f
Vedant Vashishtha <aiml202271@global.org.in>
2025-04-28 13:20:28 +0530

    Add files via upload

---
+++ recursion1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+ void recur1(int i,int n,string name){
+    if(i>=n) return;
+    cout<<name<<" ";
+    recur1(i+1,n,name); 
+ }
+
+ int main(){
+    int n;
+    cout<<"Enter value for n:";
+    cin>>n;
+    string name;
+    cout<<"Enter the name:";
+    cin>>name;
+    recur1(0,n,name);
+ }
