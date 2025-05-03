82007679d5da4c6a56be8b0945c7541105260862
Vedant Vashishtha <aiml202271@global.org.in>
2025-05-03 12:44:06 +0530

    Update recursion3.cpp

--- recursion3.cpp
+++ recursion3.cpp
@@ -1,14 +1,17 @@
+//This program will print numbers from n to 1 linearly using recursion.
+
 #include<iostream>
 using namespace std;
 
 void recur3(int n){
-    if(n<1) return;
+    if(n<1) return;//Base condition
     cout<<n<<" ";
-    recur3(n-1);
+    recur3(n-1);//recursive call
 }
 
 int main(){
     int c;
     cin>>c;
-    recur3(c);
+    recur3(c);//function call
 }
+//Output:- n.......3,2,1
